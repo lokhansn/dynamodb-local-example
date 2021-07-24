3d6442f2b4b660f15ccc2222e5cea1e64097e41f
lokhansn <lokhansn@gmail.com>
2021-07-25 00:40:34 +0530

    First Commit of the local dynamodb and spring boot example

---
+++ src/main/java/com/sgl/dynamodblocalexample/DynamodbLocalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sgl.dynamodblocalexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbLocalExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamodbLocalExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sgl/dynamodblocalexample/DynamodbLocalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgl.dynamodblocalexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbLocalExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
